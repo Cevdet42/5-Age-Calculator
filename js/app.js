c2743160f58c814c7ebeaf3fac6c80fc7cdf7046
Cevdet42 <cevmert38@gmail.com>
2022-07-30 18:57:02 +0200

    Finished the age calculator.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("connected")
+function birthyear2age() {
+    event.preventDefault();
+
+    var birth_year;
+    var current_year;
+    var age;
+
+    var d=new Date();
+
+    birth_year= document.getElementById("birth_year").value
+    current_year = d.getFullYear();
+
+    age = current_year - birth_year
+    document.getElementById("result").innerHTML = "Your age is:" + age  
+}
